d9f48bfee94f28fe5a9805967700c5fe7f40f865
kquah89 <kevin.quah@kellogg.northwestern.edu>
2020-06-08 08:38:35 +0000

    First_upload

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
-require "sinatra"                                                                     #
+require "sinatra"  
+require "sinatra/cookies"                                                             #
 require "sinatra/reloader" if development?                                            #
 require "sequel"                                                                      #
 require "logger"                                                                      #
-require "twilio-ruby"                                                                 #
-require "geocoder"                                                                    #
 require "bcrypt"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
@@ -15,5 +14,97 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+schools_table = DB.from(:schools)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+
+before do
+    # SELECT * FROM users WHERE id = session[:user_id]
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+# Home page (all events)
+get "/" do
+    # before stuff runs
+    @schools = schools_table.all
+    view "schools"
+end
+
+# Show a single event
+get "/schools/:id" do
+    @users_table = users_table
+    # SELECT * FROM events WHERE id=:id
+    @school = schools_table.where(:id => params["id"]).to_a[0]
+    # SELECT * FROM rsvps WHERE event_id=:id
+    @reviews = reviews_table.where(:school_id => params["id"]).to_a
+    # SELECT COUNT(*) FROM rsvps WHERE event_id=:id AND going=1
+    @count = reviews_table.where(:school_id => params["id"]).count
+    #@count = reviews_table.where(:school_id => params["id"], :going => true).count
+    # google api
+
+@lat_long = 42,-82
+    view "school"
+end
+
+# Form to create a new RSVP
+get "/schools/:id/reviews/new" do
+    @school = schools_table.where(:id => params["id"]).to_a[0]
+    view "new_review"
+end
+
+# Receiving end of new RSVP form
+post "/schools/:id/reviews/create" do
+    reviews_table.insert(:school_id => params["id"],
+                       :going => params["going"],
+                       :user_id => @current_user[:id],
+                       :comments => params["comments"])
+    @school = schools_table.where(:id => params["id"]).to_a[0]
+    view "create_review"
+end
+
+# Form to create a new user
+get "/users/new" do
+    view "new_user"
+end
+
+# Receiving end of new user form
+post "/users/create" do
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => BCrypt::Password.create(params["password"]))
+    view "create_user"
+end
+
+# Form to login
+get "/logins/new" do
+    view "new_login"
+end
+
+# Receiving end of login form
+post "/logins/create" do
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    # SELECT * FROM users WHERE email = email_entered
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        # test the password against the one in the users table
+        if BCrypt::Password.new(user[:password]) == password_entered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else 
+        view "create_login_failed"
+    end
+end
+
+# Logout
+get "/logout" do
+    session[:user_id] = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,36 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :schools do
   primary_key :id
   String :title
   String :description, text: true
   String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
+  foreign_key :school_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+schools_table = DB.from(:schools)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+schools_table.insert(title: "University of Chicago (Booth)", 
+                    description: "The University of Chicago Booth School of Business (also known as Chicago Booth, or Booth) is the graduate business school of the University of Chicago in Chicago, Illinois. The University of Chicago, including Booth faculty, has produced more Nobel laureates in the Economic Sciences than any other school. Formerly known as The University of Chicago Graduate School of Business, Chicago Booth is the second-oldest business school in the U.S. The Full-Time MBA Program is currently tied for third with Harvard Business School and the MIT Sloan School of Management according to U.S. News & World Report.",
+                    # date: "June 21",
+                    location: "Chicago, Illinois, United States")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+schools_table.insert(title: "Northwestern University (Kellogg)", 
+                    description: "The Kellogg School of Management at Northwestern University (also known as Kellogg) is the business school of Northwestern University, located in the Chicago Metropolitan Area, Illinois. Founded in 1908, Kellogg pioneered the use of group projects and evaluations and popularized the importance of team leadership within the business world. Kellogg's 2-Yr MBA Program is currently ranked #3 in the U.S. according to U.S. News & World Report and Forbes Magazine and #4 globally according to The Economist.",
+                    #date: "July 4",
+                    location: "Evanston, Illinois, United States")
